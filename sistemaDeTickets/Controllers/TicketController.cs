70b253ba2e1659961fd34b91f54e57fea8b05119
Joaquin Rios <joaquin.rios@virginiogomez.cl>
2023-05-18 00:44:25 -0400

    Agregar archivos de proyecto.

---
+++ sistemaDeTickets/Controllers/TicketController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using sistemaDeTickets.Models;
+
+namespace sistemaDeTickets.Controllers
+{
+    public class TicketController : Controller
+    {
+        private static List<SupportTicket> _data = new List<SupportTicket>()
+        {
+            new SupportTicket{ Subject="Error solicitud en DB", Description="Muestra en pantalla error XLM y no permite acceder a la informacion solicitada, esto ocurre al intentar acceder al listado de pacientes", CreatedDate=DateTime.Now.AddDays(-2)}
+        };
+
+
+        public IActionResult Index()
+        {
+            List <SupportTicket> ticketsAbiertos = new List<SupportTicket>();
+            foreach (SupportTicket ticket in _data)
+            {
+                if (ticket.isOpen)
+                {
+                    ticketsAbiertos.Add(ticket);
+                }
+            }
+            return View(ticketsAbiertos);
+        }
+
+        public IActionResult NuevoTicket()
+        {
+            return View();
+        }
+
+        public IActionResult Guardar(SupportTicket ticket)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View("nuevoTicket");
+            }
+            _data.Add(ticket);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult ticketCerrado()
+        {
+            List<SupportTicket> ticketsCerrados = new List<SupportTicket>();
+            foreach (SupportTicket ticket in _data)
+            {
+                if (!ticket.isOpen)
+                {
+                    ticketsCerrados.Add(ticket);
+                }
+            }
+            return View(ticketsCerrados);
+        }
+
+        public IActionResult CerrarTicket(int ticketNumber)
+        {
+            SupportTicket ticket = _data.Find(e => e.TicketNumber == ticketNumber);
+            ticket.MarkAsResolved();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ sistemaDeTickets/Models/SupportTicket.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace sistemaDeTickets.Models
+{
+    public class SupportTicket
+    { 
+        public int TicketNumber { get; set; }
+        [Required(ErrorMessage = "El asunto es obligatorio")]
+        [MaxLength(25)]
+        public string Subject { get; set; }
+        [Required(ErrorMessage = "La descripcion es obligatorio")]
+        [MaxLength(125)]
+        public string Description { get; set; }
+        public bool isOpen { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ResolvedDate { get; set; }
+
+
+        public SupportTicket()
+        {
+            Random rnd = new Random();
+            this.TicketNumber = rnd.Next(1, 1000 + 1);
+            isOpen = true;
+            CreatedDate = DateTime.Now;
+            ResolvedDate = DateTime.MinValue;
+        }
+
+        public SupportTicket(int ticketNumber,string subject, string description)
+        {
+
+        TicketNumber = ticketNumber;
+        Subject = subject;
+        Description = description;
+        isOpen = true;
+        CreatedDate = DateTime.Now;
+        ResolvedDate = DateTime.MinValue;
+
+        }
+
+        public void MarkAsResolved()
+        {
+            isOpen = false;
+            ResolvedDate = DateTime.Now;
+        }
+    }
+}
